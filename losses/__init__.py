d821202eda360010936876713d4e4e376d74cc50
Cameron Mattson <camo@oak.ucdenver.pvt>
2026-07-01 15:50:56 -0600

    Remove stale WGAN discriminator loss code

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,3 +1,1 @@
 from .L1Loss import L1Loss
-from .WassersteinGeneratorCrossZamirskiLoss import WassersteinGeneratorCrossZamirskiLoss
-from .WassersteinGradientPenaltyLoss import WassersteinGradientPenaltyLoss
